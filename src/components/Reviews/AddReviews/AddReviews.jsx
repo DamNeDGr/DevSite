6679b8fff7460a8893a7eba6b24b2e596c4f172b
DamNeDGr <f89063431393@yandex.ru>
2025-04-03 21:14:18 +0400

    Update AddReviews.jsx

--- src/components/Reviews/AddReviews/AddReviews.jsx
+++ src/components/Reviews/AddReviews/AddReviews.jsx
@@ -19,6 +19,7 @@ export default function AddReviews({addReview}) {
             setAuthor('');
             setText('');
             setDsblBtn(false);
+            recaptchaRef.current.reset();
 
         } 
     };
