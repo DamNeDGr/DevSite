603597afedf0a7f340791c7af503e8ad652bacf2
DamNeDGr <f89063431393@yandex.ru>
2025-04-03 19:50:07 +0400

    Update

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReviewsList from './ReviewsList/ReviewsList';
+import './reviews.css'
+import AddReviews from './AddReviews/AddReviews';
+
+
+export default function Reviews() {
+    const [reviews, setReviews] = useState([
+        { id: 1, author: 'Anna', text: 'Очень полезный сайт'},
+        { id: 2, author: 'Denis', text: 'Рекомендую этот сайт'},
+    ]);
+
+    const [isAuth, setIsAuth] = useState(true);
+
+    const addReview = (author, text) => {
+        const newReview = {
+            id: reviews.length + 1,
+            author,
+            text,
+        };
+        setReviews([...reviews, newReview]);
+        
+        
+    };
+   
+    const deleteReview = (id) => {
+        setReviews(reviews.filter((review) => review.id !== id));
+    }
+  return (
+    <div className="container">
+        <div className="reviews__content">
+            <h1 className="reviews__title">Отзывы :</h1>
+        </div>
+        <ReviewsList onDelete={deleteReview} Auth={isAuth} reviews={reviews}/>
+        <AddReviews addReview={ addReview }/>
+    </div>
+  )
+}
